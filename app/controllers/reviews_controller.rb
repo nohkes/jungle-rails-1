142ec8b495852edd5328eebc792b1444fb488f1e
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-12-08 04:26:52 +0000

    added all the user specific functions ie delete to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,17 @@
 class ReviewsController < ApplicationController
+  
+    before_filter :authorize
 
   def create
     @product = Product.find(params[:product_id])
     @review = @product.review.create(review_params)
     redirect_to product_path(@product)
   end
+    def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+     redirect_to :back
+    end
    private
     def review_params
       params.require(:review).permit(:description, :rating)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show] do
-    resource :reviews, except: :index, only: [:create]
+    resource :reviews, except: :index, only: [:create, :destroy]
   end
   resources :categories, only: [:show]
 
